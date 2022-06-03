1c7b1a907e8b2e295d4eef8c746f5510921c10c9
ranchaya <ranchayasambath@gmail.com>
2022-06-02 21:54:32 -0500

    initial python course

---
+++ app.py
@@ -0,0 +1,41 @@
+age = 20
+age = 30
+price = 19.95
+first_name = "Jack"
+is_online = False
+print(age,first_name,is_online)
+print("Hello World")
+
+name = "John Smith"
+age = 20
+is_new_patient = True
+
+# name = input("What is your name? ")
+# print("Hello " + name)
+
+# birth_year = input("Enter your birth year: ")
+# age = 2022 - int(birth_year)
+# print(age)
+
+# first = input("First: ")
+# first = float(input("First: "))
+# second = input("Second: ")
+# second = float(input("Second: "))
+# sum =(int(first) + int(second))
+# sum =(float(first) + float(second))
+# sum =(first + second)
+# print("Sum: " + str(sum))
+# print("Sum:", sum)
+
+course = 'Python for Beginners'
+# print(course)
+# print(course.upper())
+# print(course.lower())
+# print(course.find('y'))
+# print(course.find('for'))
+# print(course.replace('for', '4'))
+print(course.find('Python'))
+print('Python' in course)
+
+
+
